c428f4c513d2f4324f3a3525635a067faf4fac79
helloworld20224 <160257777+helloworld20224@users.noreply.github.com>
2024-02-16 20:45:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("addTaskBtn").addEventListener("click", function () {
+  var taskInput = document.getElementById("taskInput");
+  var taskText = taskInput.value.trim();
+
+  if (taskText !== "") {
+    var taskList = document.getElementById("taskList");
+    var li = document.createElement("li");
+
+    li.textContent = taskText;
+    taskList.appendChild(li);
+    taskInput.value = "";
+  }
+});
